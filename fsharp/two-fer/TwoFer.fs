736d9f142de048d0d13631fd223d82cf1a964700
Lasse Normann Hvid <LNJ@schultz.dk>
2018-10-06 15:53:34 +0200

    Fix format warning

--- fsharp/two-fer/TwoFer.fs
+++ fsharp/two-fer/TwoFer.fs
@@ -4,6 +4,7 @@ let twoFerFunctionSyntax = function
     | Some name -> sprintf "One for %s, one for me." name 
     | None -> "One for you, one for me."
 
-let twoFer person = match person with
+let twoFer person = 
+    match person with
     | Some name -> sprintf "One for %s, one for me." name
     | None -> "One for you, one for me."
