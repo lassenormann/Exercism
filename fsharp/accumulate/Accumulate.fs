70fd64ddcf8f4643a14a90dfac93358134109f55
Lasse Normann Hvid <LNJ@schultz.dk>
2018-11-21 20:52:53 +0100

    Add solution using computationla expression

--- fsharp/accumulate/Accumulate.fs
+++ fsharp/accumulate/Accumulate.fs
@@ -1,8 +1,15 @@
 ﻿module Accumulate
 
+// Cheating using build-in list function
 let accumulate' func input = input |> List.map func
 
-let rec accumulate func input = 
+// Not tail recursive. Because expressions called is NOT the recursive call. 
+// func x must be placed
+let rec accumulate'' func input = 
         match input with
         | [] -> []
-        | x::xs -> func x::accumulate func xs
+        | x::xs -> func x::accumulate'' func xs
+
+//Using computational expressions
+let rec accumulate func input = 
+    [for n in input -> func n]
