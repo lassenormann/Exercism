e7feaf023ab8a291f07c6301b98f8063c2d161d3
Lasse Normann Hvid <LNJ@schultz.dk>
2018-11-28 21:21:46 +0100

    Add solution to collatz conjecture

---
+++ fsharp/collatz-conjecture/CollatzConjecture.fs
@@ -0,0 +1,12 @@
+﻿module CollatzConjecture
+
+let rec collatz n acc = 
+    match n with
+        | 1 -> acc
+        | n when (n % 2 = 0) -> collatz (n/2) (acc+1)
+        | n -> collatz (3*n+1) (acc+1)
+
+let steps number =
+    match number with
+        | n when n <= 0 -> None
+        | n -> Some (collatz n 0)
