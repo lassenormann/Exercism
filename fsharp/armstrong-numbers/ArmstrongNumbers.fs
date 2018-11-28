fd6ff138651f2a60f13ecb77877885f9de855f4a
Lasse Normann Hvid <LNJ@schultz.dk>
2018-11-28 23:44:01 +0100

    Improve calc list of digits in armstrong numbers

--- fsharp/armstrong-numbers/ArmstrongNumbers.fs
+++ fsharp/armstrong-numbers/ArmstrongNumbers.fs
@@ -1,17 +1,23 @@
 ﻿module ArmstrongNumbers
 
-let digits n = 
-    let rec digi pos = 
-        if n < pos 
-        then []
-        else (n%(pos*10))/pos::digi (pos*10)
-    digi 1
+// let digits' n = 
+//     let rec digi pos = 
+//         if n < pos 
+//         then []
+//         else (n%(pos*10))/pos::digi (pos*10)
+//     digi 1
 
-let numberOfDigits n =  n |> digits |> List.length
+// let numberOfDigits n =  n |> digits |> List.length
+
+let digits number = 
+    let rec digitsRec ds = function 
+        | d when d < 10 -> d::ds
+        | d -> digitsRec ((d%10)::ds) (d/10)
+    digitsRec [] number
 
 let isArmstrongNumber number = 
-    let numberOfDigits = numberOfDigits number
-    number
-    |> digits
-    |> List.sumBy (fun n -> pown n numberOfDigits)
-    |> (=)  number
+    let digits = digits number
+    let numberOfDigits = digits |> List.length
+    digits
+    |> List.sumBy (fun d -> pown d numberOfDigits)
+    |> (=) number
