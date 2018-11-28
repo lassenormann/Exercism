5ade63dcc98c0aad2fa3723617226a6035dc2047
Lasse Normann Hvid <LNJ@schultz.dk>
2018-11-28 23:08:54 +0100

    Add solution to armstrong numbers

---
+++ fsharp/armstrong-numbers/ArmstrongNumbers.fs
@@ -0,0 +1,17 @@
+﻿module ArmstrongNumbers
+
+let digits n = 
+    let rec digi pos = 
+        if n < pos 
+        then []
+        else (n%(pos*10))/pos::digi (pos*10)
+    digi 1
+
+let numberOfDigits n =  n |> digits |> List.length
+
+let isArmstrongNumber number = 
+    let numberOfDigits = numberOfDigits number
+    number
+    |> digits
+    |> List.sumBy (fun n -> pown n numberOfDigits)
+    |> (=)  number
