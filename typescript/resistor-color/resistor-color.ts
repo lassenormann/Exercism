22109284fca8edb604eae4eebfbb4f158dddd368
Lasse Normann Hvid <LNJ@schultz.dk>
2021-04-26 12:58:36 +0200

    Resistor color

---
+++ typescript/resistor-color/resistor-color.ts
@@ -0,0 +1,28 @@
+export type color =
+  | "black"
+  | "brown"
+  | "red"
+  | "orange"
+  | "yellow"
+  | "green"
+  | "blue"
+  | "violet"
+  | "grey"
+  | "white";
+
+const colorValues: Record<color, number> = {
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5,
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9,
+};
+
+export const colorCode = (color: color): number => colorValues[color];
+
+export const COLORS = Object.keys(colorValues);
